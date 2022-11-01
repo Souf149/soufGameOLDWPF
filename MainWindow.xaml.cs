99a3f2539711bb266ff0a37e0f66baa7fde7efc7
Soufyan <MetaByte149@gmail.com>
2022-11-01 21:49:19 +0200

    added sprite to window and made window transparent

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace soufGame
 {
@@ -20,9 +21,45 @@ namespace soufGame
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        Random rnd = new Random();
+
+        //List<ImageBrush> sprites = new List<ImageBrush>();
+        ImageBrush playerSprite = new ImageBrush();
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameCanvas.Focus();
+            gameTimer.Tick += Update;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+
+            playerSprite.ImageSource = new BitmapImage(new Uri(@"pack://application:,,,/assets\adventurer-sprites.png"));
+            AddPlayer();
+
+        }
+
+        private void Update(object? sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddPlayer()
+        {
+
+            Rectangle rect = new Rectangle();
+            var a = testRect;
+            testRect.Fill = playerSprite;
+
+
+
+            //gameCanvas.Children.Add(sprite);
+            //Canvas.SetTop(sprite, 0);
+            //Canvas.SetLeft(sprite, 0);
         }
     }
 }
